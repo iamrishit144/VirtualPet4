dab6050b921cd1c9d83e6d0dc6e191977745b853
iamrishit144 <77106649+iamrishit144@users.noreply.github.com>
2021-05-30 19:49:42 +0530

    Update Food.js

--- Food.js
+++ Food.js
@@ -1,30 +1,31 @@
 class Food{
     constructor(){
-        this.foodStock = 0
-        this.lastfed
-        this.image = loadImage("images/Milk.png")
+        this.foodStock = 10;
+        this.lastfed;
+        this.image = loadImage("images/Milk.png"); 
+        this.image = preloadImage("images/Milk.png");
     }
 
     getfoodStock(){
 
-        return this.foodStock
+        return this.foodStock;
     
     }
 
     updatefoodStock(foodStock){
-        this.foodStock = foodStock
+        this.foodStock = foodStock;
 
     }
 
     deductFoodStock(){
 
     if(this.foodStock>0){
-     this.foodStock = this.foodStock+1
+     this.foodStock = this.foodStock+1;
     }
 
     }
 
-    display()
+    display();
     
 
 }
